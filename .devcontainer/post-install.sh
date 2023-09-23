ebd11b26d5e01517cfcf689689ac91f35c13dfc7
MohamedAnouar <43519543+MohamedAnouar@users.noreply.github.com>
2023-09-23 22:29:13 +0000

    update precommit step

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -23,4 +23,5 @@ wget "https://releases.hashicorp.com/terraform/"$VERSION"/terraform_"$VERSION"_l
 unzip "terraform_"$VERSION"_linux_amd64.zip"
 sudo install terraform /usr/local/bin/
 pip install pre-commit
+pre-commit install
 go install github.com/terraform-docs/terraform-docs@v0.16.0
